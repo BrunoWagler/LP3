b44a20f452e1b5cf6f50b979d3ed98e2103e0b46
07874741188 <reidx0@Outlook.com>
2025-01-24 10:15:46 -0400

    Adicionado filtragem de autor

--- src/main/java/repository/LivroRepository.java
+++ src/main/java/repository/LivroRepository.java
@@ -3,24 +3,30 @@ package repository;
 import model.LivroModel;
 
 import javax.persistence.*;
-import javax.swing.*;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class LivroRepository {
+public class LivroRepository
+{
     private static LivroRepository instance;
     protected EntityManager entityManager;
     public LivroRepository() {
         entityManager = getEntityManager();
     }
-    private EntityManager getEntityManager() {
+    private EntityManager getEntityManager()
+    {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("crudHibernatePU");
-        if (entityManager == null) {
+        if (entityManager == null)
+        {
             entityManager = factory.createEntityManager();
         }
         return entityManager;
     }
-    public static LivroRepository getInstance() {
-        if (instance == null) {
+    public static LivroRepository getInstance()
+    {
+        if (instance == null)
+        {
             instance = new LivroRepository();
         }
         return instance;
@@ -40,4 +46,46 @@ public class LivroRepository {
            return e.getMessage();
         }
     }
+
+    public List<LivroModel> buscarLivros()
+    {
+        try
+        {
+            List<LivroModel> livros = entityManager.createQuery("From LivroModel").getResultList();
+            return livros;
+        }
+        catch (Exception e)
+        {
+            return new ArrayList<>();
+        }
+    }
+
+    public List<LivroModel> buscarLivrosAutor(String autor)
+    {
+        try
+        {
+            List<LivroModel> livros = entityManager.createQuery("From LivroModel where autor like '%"+autor+"%'").getResultList();
+            return livros;
+        }
+        catch (Exception e)
+        {
+            return new ArrayList<>();
+        }
+    }
+
+    public String Remover(LivroModel livro)
+    {
+        try
+        {
+            entityManager.getTransaction().begin();
+            entityManager.remove(livro);
+            entityManager.getTransaction().commit();
+            return "Removido";
+        }catch(Exception e)
+        {
+            return e.getMessage();
+        }
+
+    }
+
 }
